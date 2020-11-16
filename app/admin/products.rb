a0d879adfb986045cf8267d6c68131b34f1fd272
Geoff Hodgson <geoffphodgson1@gmail.com>
2020-11-16 17:30:48 -0600

    Added Product admin Resource

---
+++ app/admin/products.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register Product do
+  permit_params :name, :price, :stock, :catagory_id, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
+end
