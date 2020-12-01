d540c24e5d416d90fd1b4c7f5153c7d915d4da32
Geoff Hodgson <geoffphodgson@gmail.com>
2020-11-30 22:24:19 -0600

    Added Footer + added menus to Index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.new_products.limit(5).order("created_at")
+    @new_products = Product.new_products.limit(5).order("created_at")
+    @expensive_games = Product.all.order("price DESC").limit(4)
+    @rare_games = Product.all.order("stock ASC").limit(4)
   end
 end
