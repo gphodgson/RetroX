20cad7900efec621c0becd190402e1991e2362d0
Geoff Hodgson <geoffphodgson@gmail.com>
2020-12-08 15:33:16 -0600

    fixes?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :init_session
   helper_method :cart
   helper_method :cart_subtotal
+  helper_method :init_session
 
   private
 
