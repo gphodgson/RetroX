314d2ec71e0358a75d754503064b195a4651a87b
Geoff Hodgson <geoffphodgson@gmail.com>
2020-12-08 16:06:53 -0600

    fixes?4

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,7 @@ OrderedItem.delete_all
 if Rails.env.development?
   AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
 end
+
+if Rails.env.production?
+  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+end
